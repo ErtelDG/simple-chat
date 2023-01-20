6f155c1089de337a77f525aca9778ad210bf21a4
ErtelDG <ertel.developer@t-online.de>
2023-01-20 17:22:11 +0100

    loginNewModels

--- first_django_app/chat/apps.py
+++ first_django_app/chat/apps.py
@@ -1,6 +1,8 @@
+# The default_auto_field attribute is set to 'django.db.models.BigAutoField'
 from django.apps import AppConfig
 
 
+# The default_auto_field attribute is set to the BigAutoField class
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
